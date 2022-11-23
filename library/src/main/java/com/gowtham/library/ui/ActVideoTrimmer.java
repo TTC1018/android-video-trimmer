7fb15ad5d219f134eca4b538aaaf2f5855c618e5
DongHyeon Park <paramore@kakao.com>
2022-11-23 17:24:07 +0900

    fix: 티라미수 호환성 해결 시도

--- library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
+++ library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
@@ -692,7 +692,7 @@ public class ActVideoTrimmer extends LocalizationActivity {
     private boolean checkStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             return checkPermission(
-                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_MEDIA_LOCATION);
+                    Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.ACCESS_MEDIA_LOCATION);
         } else
             return checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
 
